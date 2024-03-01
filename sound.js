7f520342aadfcae1774503110f1500d99834bd77
victorochoa001 <142928042+victorochoa001@users.noreply.github.com>
2024-03-01 18:08:11 +0000

    Fri, Mar 1, 2024, 10:08 AM -08:00

--- sound.js
+++ sound.js
@@ -1,12 +1,21 @@
-sounds = ['applause', 'boo', 'gasp, 'tada', 'victory', 'wrong'] 
+sounds = ['applause', 'boo', 'gasp, 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText=sound
     btn.addEventListener('click', () => {
+        stopSongs()
         document.getElementById(sound).play()
     })
 
     document.getElementById('buttons').appendChild(btn)
 })
+
+function stopSongs() {
+    sound.forEach(sound => {
+        song = document.getElementbyId(sound)
+        song.pause()
+        song.currentTime = 0
+    })
+}
