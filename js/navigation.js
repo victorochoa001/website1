9f092a182bc60a93b464af5f0698051f49a3699d
victorochoa001 <142928042+victorochoa001@users.noreply.github.com>
2024-03-13 16:20:03 +0000

    Wed, Mar 13, 2024, 9:20 AM -07:00

--- js/navigation.js
+++ js/navigation.js
@@ -2,11 +2,11 @@ nav = document.querySelector('.nav')
 
 window.addEventListener('scroll', fixNav)
 
-function fixNav() {
-    console.log(window.scrollY)
-    if (window.scrollY > 223) {
+function fixNav{
+    if (window.scrollY > 223){
         nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
+    }
+    else{
+        nav.class.List.remove('active')
     }
 }
