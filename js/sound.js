dca4ff5b620208864ce82fbe248e0e7edccfa2d2
victorochoa001 <142928042+victorochoa001@users.noreply.github.com>
2024-03-13 16:11:52 +0000

    Wed, Mar 13, 2024, 9:11 AM -07:00

--- js/sound.js
+++ js/sound.js
@@ -4,31 +4,18 @@ sounds.forEach(sound => {
     btn = document.createElement('button')
     btn.classList.add('btn')
     btn.innerText=sound
-    btn.addEventListener('click', () => {
+    btn.addEventListener( 'click', () => {
         stopSongs()
         document.getElementById(sound).play()
-    })
 
+    })
     document.getElementById('buttons').appendChild(btn)
 })
 
-function stopSongs() {
+function stopSongs(){
     sounds.forEach(sound => {
         song = document.getElementById(sound)
         song.pause()
-        song.currentTime = 0
+        song.currentTime=0
     })
-}
-
-nav = document.querySelector('.nav')
-
-window.addEventListener('scroll', fixNav)
-
-function fixNav() {
-    console.log(window.scrollY)
-    if (window.scrollY > 223) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
 }
